b2a1a7e3adb7586b6362426c0b05f7e1b65162f4
qiqiboy <qiqiboy@tigerbrokers.com>
2019-04-11 00:53:06 +0800

    fix(build): disable compressing in prod env

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,10 @@ function createConfig(env, module) {
 
     return {
         input: 'src/index.js',
-        external: module === 'umd' ? ['react', 'prop-types'] : id => !id.startsWith('.') && !id.startsWith('@babel/runtime') && !path.isAbsolute(id),
+        external:
+            module === 'umd'
+                ? ['react', 'prop-types']
+                : id => !id.startsWith('.') && !id.startsWith('@babel/runtime') && !path.isAbsolute(id),
         output: {
             file: `dist/react-formutil.${module}.${env}.js`,
             format: module,
@@ -84,15 +87,18 @@ function createConfig(env, module) {
                 terser({
                     sourcemap: true,
                     output: { comments: false },
-                    compress: {
-                        warnings: false,
-                        comparisons: false,
-                        keep_infinity: true
-                    },
+                    compress:
+                        module === 'umd'
+                            ? {
+                                  warnings: false,
+                                  comparisons: false,
+                                  keep_infinity: true
+                              }
+                            : false,
                     warnings: false,
                     ecma: 5,
                     ie8: false,
-                    toplevel: false
+                    toplevel: module !== 'umd'
                 })
         ].filter(Boolean)
     };
